158a05c6ca10644270486106f8e36d1e6dff62a6
Tobi1ff <61853898+Tobi1ff@users.noreply.github.com>
2026-05-04 06:45:46 +0200

    Add HelmetProvider to App component
    Wrap the App component with HelmetProvider for better head management.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+
+function App() {
+  return (
+    <HelmetProvider>
+      {/* Your routes/components */}
+    </HelmetProvider>
+  );
+}
